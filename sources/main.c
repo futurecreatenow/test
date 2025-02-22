1760e42b8bfc4883bcb2eaba2a40a6ea363cc73c
taka6yuki28@gmail.com <taka6yuki28@gmail.com>
2025-02-22 17:35:06 +0900

    Listの表示機能の実装開始

--- sources/main.c
+++ sources/main.c
@@ -40,6 +40,10 @@ acc_node LNCreate(string);
 boolean is_Centinel(acc_node);
 boolean LNIs_Null(acc_node);
 void LNAppend(acc_node,acc_node);
+void ListPrint(list);
+boolean ListIs_Null(list);
+acc_node LNNext(acc_node);
+string LNData_Ref(acc_node);
 
 //関数の実体
 /* function to create a oneway list */
@@ -49,6 +53,7 @@ list ListCreate() {
    l->head = l->tail = CENTINEL;
    return l;
 }
+
 /*リスト先のポインタを指定せず値のみ格納した構造体*/
 acc_node LNCreate(string w){
     acc_node an;
@@ -57,36 +62,70 @@ acc_node LNCreate(string w){
     an->next = CENTINEL;
     return an;
 }
+
 boolean is_Centinel(acc_node an){
     return (an == CENTINEL) ? YES : NO;
+
 }
 boolean LNIs_Null(acc_node an){
     return is_Centinel(an);
 }
+
 void LNAppend(acc_node e,acc_node a){
     e->next = a;
 }
+
 /*function to put the node after the tail of the list*/
 list ListAppend(list l,string moji){
     acc_node t = LNCreate(moji);
     if (l == NULL_LIST){
         printf("List is never allocated");
     }else if(LNIs_Null(l->head)==YES){
+        printf("ListAppend func else if >>  %s\n",moji);
         l->head=l->tail=t;
     }else{
+        printf("ListAppend func else >>  %s\n",moji);
         LNAppend(l->tail,t);
     }
     return l;
 }
+
+boolean ListIs_Null(list l){
+    if(l == NULL_LIST) {printf("List is never allocated");}
+    else if((LNIs_Null(l->head)==YES) && (LNIs_Null(l->tail) == YES)){
+        return YES;
+    }else{return NO;}
+}
+
+acc_node LNNext(acc_node an){
+    if(is_Centinel(an)) return an;
+    else return an->next;
+}
+
+string LNData_Ref(acc_node an){ return an->w; }
+
+void ListPrint(list l){
+    acc_node n;
+    if(ListIs_Null(l) == YES){
+        printf("Null list\n");
+    }else{
+        for(n=l->head;LNIs_Null(n) == NO;n=LNNext(n)){
+            printf("%s\n",LNData_Ref(n));
+        }
+    }
+}
+
+
 int main(){
     sort_key sKey;
     sKey = BY_FREQ;
     list wordlist = ListCreate();
     int mojisize = sizeof(moji) /sizeof(moji[0]);
     for (int i = 0;i < mojisize ;i++){
-        printf("%s\n",moji[i]);
+        printf("moji >> %s\n",moji[i]);
         wordlist = ListAppend(wordlist,moji[i]);
     }
-    printf("no problem");
+    printf("-----------------");
+    ListPrint(wordlist);
     return 0;
 }
