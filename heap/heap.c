7c467d9a31699d8f26e39d94b3dff3c8ecb3a292
futurecreatenow <158742450+futurecreatenow@users.noreply.github.com>
2025-08-22 17:51:09 +0900

    Add files via upload

---
+++ heap/heap.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mydef.h>
+
+#include <heap_ds.h>
+
+static heap *heap_Space;
+
+/*	Initialization for Heap Controls				*/
+
+void HPInit() { heap_Space = NULL_HEAP; }
+
+/*	create a singleton instance of heap				*/
+
+heap *HPCreate() {
+   if (heap_Space == NULL_HEAP) {
+        int y;
+
+        heap_Space = (heap *) malloc(sizeof(heap));
+	heap_Space->next = 0;
+	for (y = 0; y < HEAP_SIZE; ++y) heap_Space->heapSpace[y] = NULLV;
+   }
+   return heap_Space;
+}
+
+/*	function to get heap space					*/
+
+void *HPAlloc(heap* thisHeap, int size) {
+   if (heap_Space->next >= HEAP_SIZE) {
+       fprintf(stderr, "Heap area is overflow.");
+       exit(-1);
+   }
+   else {
+	thisHeap->heapSpace[thisHeap->next] = malloc(size);
+	return thisHeap->heapSpace[thisHeap->next];
+   }
+}
+
+/*	Clean all Heap area						*/
+
+void HPFree_All(heap *thisHeap) {
+    int y;
+
+    for (y = 0; y < thisHeap->next; ++y) {
+       if (thisHeap->heapSpace[y] != NULLV) {
+	  free(thisHeap->heapSpace[y]);
+	  thisHeap->heapSpace[y] = NULLV;
+      }
+    }
+    thisHeap->next = 0;
+}
+
+/*	FreeSpace							*/
+
+void HPFree(heap *thisHeap, void *allocatedArea) {
+   int y;
+
+   for (y = 0; y < thisHeap->next; ++y) {
+      if (thisHeap->heapSpace[y] == allocatedArea) {
+         free(thisHeap->heapSpace[y]);
+	 thisHeap->heapSpace[y] = NULLV;
+	 return;
+      }
+   }
+}
---
+++ heap/heap.h
@@ -0,0 +1,12 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <heap_ds.h>
+
+extern void HPInit(void);
+extern heap *HPCreate(void); /* singleton */
+extern void HPFree(heap *, void *);
+extern void *HPAlloc(heap*, int);
+extern void HPFree_All(heap *);
+
+#endif
